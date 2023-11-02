a77828eb4b02de102cf2f390f82df2efbf7ab833
Giacomo Sarrocco <git@sarr.co>
2023-11-02 16:07:59 +0100

    Set up search stations view with api interaction

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
+import 'search_stations/station_details_view.dart';
+import 'search_stations/search_stations_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
@@ -69,11 +69,11 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  case StationDetailsView.routeName:
+                    return const StationDetailsView();
+                  case SearchStationsListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const SearchStationsListView();
                 }
               },
             );
--- lib/src/search_stations/station_details_view.dart
+++ lib/src/search_stations/station_details_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 /// Displays detailed information about a SampleItem.
-class SampleItemDetailsView extends StatelessWidget {
-  const SampleItemDetailsView({super.key});
+class StationDetailsView extends StatelessWidget {
+  const StationDetailsView({super.key});
 
   static const routeName = '/sample_item';
 
