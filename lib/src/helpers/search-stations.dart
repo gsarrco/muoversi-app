5800fe455a17981982916dada6c6e17e300ddc5a
Giacomo Sarrocco <git@sarr.co>
2024-01-28 08:43:39 +0100

    Replace parameter onlySource with sources in searchStations api and update tests and searchStationsAndHide

--- lib/src/helpers/search-stations.dart
+++ lib/src/helpers/search-stations.dart
@@ -3,11 +3,11 @@ import 'package:http/http.dart' as http;
 import '../models/station.dart';
 import 'api.dart';
 
-Future<List<Station>> searchStationsAndHide(
-    http.Client client, String query, int maxLimit, int slice,
-    [String? onlySource, List<String>? hideIds]) async {
+Future<List<Station>> searchStationsAndHide(http.Client client, String query,
+    int maxLimit, int slice, List<String> sources,
+    [List<String>? hideIds]) async {
   List<Station> stations =
-      await searchStations(client, query, maxLimit, onlySource);
+      await searchStations(client, query, maxLimit, sources);
   if (hideIds != null && hideIds.isNotEmpty) {
     stations.removeWhere((station) => hideIds.contains(station.id));
   }
